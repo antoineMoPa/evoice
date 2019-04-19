060e98f915c06aaf57221aa1e9803145473f7748
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2019-04-19 11:46:13 -0400

    add comments

--- evoice.lisp
+++ evoice.lisp
@@ -12,5 +12,15 @@
    )
   )
 
+;; You can use replace-key to call a shortcut whenever you type something
+;; or say something.
+;; Since google voice can't send "CTRL", it can be useful to setup phrases like "line begin"
+(replace-key "line begin" "\C-a")
+
+;; Other shortcut ideas:
+;; In termux, typing CTRL+a is slow, but typing " aa" is faster:
 (replace-key " aa" "\C-a")
 (replace-key " kk" "\C-k")
+
+
+
